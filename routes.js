f23ef7110571e52a0ac329021451309eaf51b22a
MatthewC221 <matthewchen42@gmail.com>
2018-11-07 23:20:20 +0000

    the code is now sent in XMLHttprequest, and shell execute is ok

--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@ var bodyParser      = require('body-parser');
 var https           = require('https');
 var path_module     = require('path');
 var session         = require('express-session');
+var shell           = require('shelljs');
 
 var routes = express.Router({ mergeParams: true });
 var path = __dirname + '/views/';
@@ -15,6 +16,21 @@ routes.get("/", function (req, res) {
 
 routes.get("/code", function (req, res) {
     res.render("code");
+});
+
+routes.post("/run", function (req, res) {
+    // Will only run once
+    var stringCode;
+    for (var key in req.body) {
+        stringCode = key;
+    }
+    console.log(stringCode);
+    var exec = require('child_process').exec, child;
+
+    // const shell = require('shelljs');
+     //shell.exec(comandToExecute, {silent:true}).stdout;
+     //you need little improvisation
+     shell.exec('dir')
 });
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var express = require('express');
 
 var app = express();
 
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/styles', express.static(__dirname + '/styles'));
